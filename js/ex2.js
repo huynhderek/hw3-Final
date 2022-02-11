fe266675a28813af788e21538d59a9cb7e0eaf81
Derek Huynh <derekhuynh8791@gmail.com>
2022-02-10 16:16:16 -0800

    commit

--- js/ex2.js
+++ js/ex2.js
@@ -3,3 +3,29 @@
 */
 
 console.log("Derek's Output for #2 on homework");
+
+let dayEntered = prompt("Enter a day of the week: ");
+
+console.log('You entered: ${dayEntered}');
+//Make a switch statement to set the day entered to lowercase. 
+switch(dayEntered.toLowerCase())  {
+   case "mon" : //The case set for user's input to output the next coming day
+      nextDay = "tue";
+      break;
+   case "tue": 
+      nextDay = "wed";
+      break;
+   case "wed": 
+      nextDay = "thu";
+      break;   
+   case "thu": 
+      nextDay = "fri";
+      break;
+   case "sat": 
+      nextDay = "sun";
+      break;
+   case "sun": 
+      nextDay = "mon";
+      break;
+}
+console.log('The following day is: ${nextDay}.');
