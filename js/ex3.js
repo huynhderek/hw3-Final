0447af92195234e816f3f5b80cd0d7a8540b9579
Derek Huynh <derekhuynh8791@gmail.com>
2022-02-10 17:12:05 -0800

    Create ex3.js

---
+++ js/ex3.js
@@ -0,0 +1,13 @@
+//Grab two whole numbers from user
+console.log("Derek's output for Problem 3");
+
+let num1 = Number(promopt("Enter the first whole number: "));
+let num2 = Number(promopt("Enter the second whole number: "));
+
+//Have output in form of addition, subtraction,
+//multiplication, division, and modulo
+console.log(`Addition: ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`Subtraction: ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`Multiplication: ${num1} * ${num2} = ${num1 * num2}`);
+console.log(`Division: ${num1} / ${num2} = ${num1 / num2}`);
+console.log(`Modulo: ${num1} % ${num2} = ${num1 % num2}`);
